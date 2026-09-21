25c931b57509012856402a4a6911a911175f7806
Sakshi Mishra <sakshikumarinke9@gmail.com>
2026-09-21 22:46:55 +0530

    feat: add health and readiness endpoints

--- urls.py
+++ urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from health import healthz, readyz
 
 urlpatterns = [
+    path("healthz/", healthz, name="healthz"),
+    path("readyz/", readyz, name="readyz"),
     path("admin/", admin.site.urls),
     path(
         "login/",
